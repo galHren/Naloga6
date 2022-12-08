0017606f68cad77b1f9e2a2e49a338a54e17e1cb
galHren <94851478+galHren@users.noreply.github.com>
2022-12-08 18:21:13 +0100

    Create naloga6.sh

---
+++ naloga6.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir Windows
+cd Windows
+mkdir Desktop
+mkdir Documents
+mkdir Downloads
+mkdir Pictures
+mkdir Videos
+mkdir Music
+
+for i in 1 2 3 4 5
+do
+  mkdir folder$i
+done
+
