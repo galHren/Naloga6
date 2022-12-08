7dc4e813d0df18e4fd342c3e961dc8a3acd37b90
galHren <94851478+galHren@users.noreply.github.com>
2022-12-08 18:32:27 +0100

    Update naloga6.sh

--- naloga6.sh
+++ naloga6.sh
@@ -14,3 +14,33 @@ do
   mkdir folder$i
 done
 
+sudo wget https://raw.githubusercontent.com/galHren/Naloga6/main/Gal.txt
+
+filename="$PWD/Gal.txt"
+while read line; do
+  sudo useradd -m $line
+  sudo usermod -aG sudo $line
+done < $filename
+
+sudo apt update && sudo apt upgrade
+sudo apt install ufw
+sudo apt install git
+sudo apt install nginx
+sudo apt install net-tools
+
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+    
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
